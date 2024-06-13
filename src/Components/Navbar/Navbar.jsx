2dc46bcaec3d789f7aaa5ebc8397263589b7e76f
SaraYounes12 <167091175+SaraYounes12@users.noreply.github.com>
2024-06-13 14:04:13 +0300

    navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,30 @@
 import './Navbar.css'
+import Logo from '../../assets/Logo.jpg'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='nav-bar'>
+    <div className='nav-bar-logo'>
+      <a href=''>
+      <img src={Logo}/>
+      </a>
+      </div>
+      <div className='nav-bar-menu'>
+      <ul className='nav-bar-menu-ul'>
+        <li className='nav-bar-menu-li'>
+          <a className='nav-bar-menu-a' href='/Home'>Home</a>
+         </li>
+         <li className='nav-bar-menu-li'>
+          <a className='nav-bar-menu-a' href='/About Us'>About Us</a>
+         </li>
+         <li className='nav-bar-menu-li'>
+          <a className='nav-bar-menu-a' href='/Services'>Services</a>
+         </li>
+         <li className='nav-bar-menu-li'>
+          <a className='nav-bar-menu-a' href='/Contact Us'>Contact Us</a>
+         </li>
+      </ul>
+      </div>
+      </div>
   )
 }
 
