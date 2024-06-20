5368c02386946b8fc89312048dc269b4bcf29486
SaraY252 <sarayounis252@gmail.com>
2024-06-20 11:41:13 +0300

    Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,52 +4,47 @@ import Logo from '../../assets/Logo.png'
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='first-sec'>
-      <div className='logo'>
-        <a href='/#'>
-        <img src={Logo} alt="Logo"/>
-          </a>
-      </div>
-      <div className='sections'>
-        <h3>Sections</h3>
-          <a href='../Home'>
-            Home
-          </a>
-          <a href='/#'>
-            About Us
-          </a>
-          <a href='/#'>
-            Services
-          </a>
-          <a href='/#'>
-            Contact Us
-          </a>          
-      </div>
-      <div className='links'>
-        <h3>Social Media</h3>
-          <a href='../Home'>
-            Facebook
-          </a>
-          <a href='/#'>
-             Instagram
-          </a>
-          <a href='/#'>
-            Twitter
-          </a>        
-      </div>
+     <div className='first-sec'>
+            <div className='Logo'>
+              <a href='/#'>
+              <img src={Logo} alt="Logo"/>
+                </a>
+            </div>
+         <div className='main-links'>
+            <div className='links'>
+              
+                <h2>Sections</h2>
+                  <a href='/#'>
+                    About Us
+                  </a>
+                  <a href='/#'>
+                    Services
+                  </a>
+                  <a href='/#'>
+                    Contact Us
+                  </a>  
+            </div>        
+            <div className='links'>
+              <h2>Media</h2>
+              <a href='/#'>
+                  Twitter
+                </a>  
+                <a href='/#'>
+                  Facebook
+                </a>
+                <a href='/#'>
+                  Instagram
+                </a>   
+            </div>
+        </div>
       </div>
       <div className='second-sec'>
-      <div className='copyrights'>
         <p>
-        © 2024 all rights reserved | Skin Tonic
-        </p>
-        <p>
-        SITEMANAGERV4.0
-        <img src={In2Info} alt='In2Info'/>
+        © 2024 all rights reserved | Skin Tonic | SITEMANAGER V4.0
+       <a href='http://www.in2info.com/' target='blank'> <img src={In2Info} alt='In2Info'/> </a> 
         </p>
       </div>
       </div>
-    </div>
   )
 }
 
