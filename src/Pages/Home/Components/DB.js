0b4d82fd2941d9166b061a826818e836e0e3f76e
SaraY252 <sarayounis252@gmail.com>
2024-07-01 13:30:42 +0300

    Slider

--- src/Pages/Home/Components/DB.js
+++ src/Pages/Home/Components/DB.js
@@ -1,6 +1,6 @@
 import image1 from '../../../assets/slider1.jpg'
 import image2 from '../../../assets/slider2.jpg'
-import image3 from '../../../assets/slider3.jpg'
+import image3 from '../../../assets/slider4.jpg'
 
 const imageData = [
     { src: image1 },
